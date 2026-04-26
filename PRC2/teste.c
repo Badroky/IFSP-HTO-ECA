4b261ff5727f3778fd1ae2e46beef0bc0f939296
Badroky <162215674+Badroky@users.noreply.github.com>
2026-04-26 19:21:55 +0000

    modified:   PRC2/teste.c

--- PRC2/teste.c
+++ PRC2/teste.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char tipo [25];
+    float valor_atual;
+    float limite_maximo;
+    float limite_minimo;
+    int status; // 0 - normal, 1 - alerta, 2 - crítico.
+} sensor;
+
+sensor 
 
 int main() {
     printf("Olá, ECA IFSP-HTO!\n");
