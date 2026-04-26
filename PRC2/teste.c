8c468abb22633161222b12a4a72304ba7629bb1a
Badroky <162215674+Badroky@users.noreply.github.com>
2026-04-26 19:45:03 +0000

    modified:   PRC2/teste.c

--- PRC2/teste.c
+++ PRC2/teste.c
@@ -10,7 +10,20 @@ typedef struct {
     int status; // 0 - normal, 1 - alerta, 2 - crítico.
 } sensor;
 
-sensor 
+sensor sensores[10];
+int sensores_cadastrados = 0;
+
+void cadastrar (sensor v[], int *sensores_cadastrados) {
+    if (*sensores_cadastrados >= 10) {
+        printf("Limite de Sensores cadastrados atingidos.\n");
+        return;
+    }
+    sensor a;
+    printf("Digite o ID do sensor: "); scanf("%d", &a.id);
+    printf("Digite o tipo de sensor: "); scanf("%s", a.tipo);
+
+    a.status = 0;
+}
 
 int main() {
     printf("Olá, ECA IFSP-HTO!\n");
