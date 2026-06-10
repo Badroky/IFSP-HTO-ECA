7d5b0b0830ea29f66371f1998f8191ac2f1e747e
Badroky <162215674+Badroky@users.noreply.github.com>
2026-06-10 19:48:16 +0000

    new file:   PRC2/2026/06/10/main1.c

---
+++ PRC2/2026/06/10/main1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define MAX 10
+
+typedef struct {
+    int topo;
+    int dados [MAX];
+    
+} PILHA;
+
+void inicializar_pilha (PILHA *p){
+    p -> topo = -1;
+}
+
+int pilha_vazia (PILHA *p){
+    return p -> topo == -1;
+}
+
+int pilha_cheia (PILHA *p){
+    return p -> topo == MAX - 1;
+}
+
+void insere(PILHA *p, int valor){
+    if (pilha_cheia(p)){
+        printf("[ERRO] Pilha cheia.\n");
+        return;
+    }
+    
+    p -> dados[++p -> topo] = valor;
+
+    printf("Valor %d inserido na pilha.\n", valor);
+}
+
+int remover (PILHA *p){
+    if (pilha_vazia(p)){
+        printf("[ERRO] Pilha vazia.\n");
+        return -1;
+    }
+
+    return p -> dados[p -> topo--];
+
+}
+
+void imprimir_pilha (PILHA *p){
+    printf("Topo -> ");
+
+    for (int i = p -> topo; i >= 0; i--){
+        printf("%d ", p -> dados[i]);
+    }
+
+    printf("\n\n");
+}
